5efb348cf526f87b1f8100ee0886428c7cde3e8c
cpplv <aliepins@inbox.lv>
2022-03-19 09:31:41 +0200

    Hello world

--- WindowsFormsApp20220319/Form1.cs
+++ WindowsFormsApp20220319/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp20220319
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World!";
+        }
     }
 }
